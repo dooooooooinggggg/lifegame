30d1a3d1347db10a9056bef9e7465ba6b4a0244e
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-01-31 03:01:28 +0900

    update

---
+++ 03_if/test.s
@@ -0,0 +1,69 @@
+
+section .data
+on     db "0" ;
+off    db 0x2d; <- "-"
+; err    db "e"
+return db 0x0a
+
+
+
+section .text
+global main
+
+main:
+
+; if (a < b) {
+;     a = b;
+; }
+
+; a = 1
+; b = 2
+; だとすると
+
+    mov     rax, 1
+    mov     rbx, 2
+    cmp     rax, rbx
+    jge     false
+    mov     rbx, rax
+
+    ; 0の表示
+    ; write(1, string, 1)
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, on
+    mov rdx, 1
+    syscall
+    ; \nの表示
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, return
+    mov rdx, 1
+    syscall
+
+
+    ; exit(0)
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+false:
+
+    ; -の表示
+    ; write(1, string, 1)
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, off
+    mov rdx, 1
+    syscall
+    ; \nの表示
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, return
+    mov rdx, 1
+    syscall
+
+    ; exit(0)
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
